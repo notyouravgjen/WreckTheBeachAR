56386ba369cb42b81a13f68804816a43a09bf967
Jen <notyouravgjen@users.noreply.github.com>
2023-08-02 13:27:51 -0700

    waves

---
+++ Assets/Wreck the Beach assets/Assets/Scripts/WaterLogs.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class WaterLogs : MonoBehaviour {
+    public int size = 50;
+    public float momentumLossPerUpdate;
+    public Transform waveObject;
+    public float waveSpacing;
+    public KeyCode wavingKey;
+    public int numOfFixedUpdatesPerWaveUpdate;
+    private int currFixedUpdateCount;
+    private Waveable waveNumbers;
+    private Transform[] waves;
+    public float[] singleWave;  // form of a single wave, as positive or negative offsets from the transform.position.y
+
+    // Use this for initialization
+    void Start () {
+        currFixedUpdateCount = 0;
+        waveNumbers = new Waveable(size, momentumLossPerUpdate);
+        waves = new Transform[size];
+        for(int i=0; i < size; i++)
+        {
+            float xOffset = i * waveSpacing;
+            waves[i] = Instantiate(waveObject, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z), Quaternion.Euler(90, 0, 0), transform);
+        }
+	}
+	
+	void FixedUpdate () {
+		if(Input.GetKeyDown(wavingKey) && !GameManager.instance.interactionDisabled)
+        {
+            waveNumbers.AddWave(singleWave);
+        }
+
+        currFixedUpdateCount++;
+        if (currFixedUpdateCount >= numOfFixedUpdatesPerWaveUpdate)
+        {
+            EventManager.BroadcastDamageThings(waveNumbers.GetOffset(size-1));
+            waveNumbers.Update();
+            for (int i = 0; i < size; i++)
+            {
+                Vector3 wavePos = waves[i].transform.position;
+                wavePos.y = transform.position.y + waveNumbers.GetOffset(i);
+                waves[i].transform.position = wavePos;
+            }
+            currFixedUpdateCount = 0;
+        }
+	}
+
+	public void Reset()
+	{
+		this.waveNumbers = new Waveable(size, momentumLossPerUpdate);
+	}
+}
---
+++ Assets/Wreck the Beach assets/Assets/Scripts/Waveable.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waveable {
+
+    public int size;
+    public float momentumLossPerUpdate;
+    private Queue<float> currWave;
+    private RandomAccessCircularArray<float> heightOffsetsTravelingRight; // Delta height from defaultHeight that should moving right
+    private RandomAccessCircularArray<float> heightOffsetsTravelingLeft;  // Delta height from defaultHeight that is moving left
+
+	//Constructor
+	public Waveable (int size, float momentumLossPerUpdate) {
+        this.size = size;
+        this.momentumLossPerUpdate = momentumLossPerUpdate;
+        heightOffsetsTravelingRight = new RandomAccessCircularArray<float>(size, 0);
+        heightOffsetsTravelingLeft = new RandomAccessCircularArray<float>(size, 0);
+        currWave = new Queue<float>();
+    }
+
+    private bool IsSameSign(float f1, float f2)
+    {
+        return (f1 > 0 && f2 > 0) || (f1 < 0 && f2 < 0);
+    }
+    // Brings f1 towards 0 by f2's value
+    private float BringTowardsZero(float f1, float f2)
+    {
+        float res = System.Math.Abs(f1 - f2);
+        // If f1 and res are the same sign, their product > 0
+        if(IsSameSign(res, f1))
+        {
+            return res;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    // Update should be called manually
+    // Does not work for edge cases (sizes of 0, 1, maybe a few more where the two waving sides overlap)
+    public void Update() {
+        for (int i=0; i< size; i++)
+        {
+            float oldLeft = heightOffsetsTravelingLeft.Get(i);
+            heightOffsetsTravelingLeft.Set(i, BringTowardsZero(oldLeft, momentumLossPerUpdate));
+            float oldRight = heightOffsetsTravelingRight.Get(i);
+            heightOffsetsTravelingRight.Set(i, BringTowardsZero(oldRight, momentumLossPerUpdate));
+        }
+        float bouncedRightWaveHeight = heightOffsetsTravelingRight.RotateRight();
+        float bouncedLeftWaveHeight = heightOffsetsTravelingLeft.RotateLeft();
+        heightOffsetsTravelingLeft.Set(size - 1, bouncedRightWaveHeight);
+        float f = 0;
+        if (currWave.Count > 0) {
+          f = currWave.Dequeue();
+        }
+        heightOffsetsTravelingRight.Set(0, f + bouncedLeftWaveHeight);
+
+    }
+
+    public float GetOffset(int index)
+    {
+        return heightOffsetsTravelingLeft.Get(index) + heightOffsetsTravelingRight.Get(index);
+    }
+
+    public void AddWave(float[] wave)
+    {
+        if (currWave.Count > 0)
+        {
+            currWave.Clear();
+        }
+        // wave is queued from the right to left since waving goes to the right
+        for (int i=wave.Length-1; i>=0; i--)
+        {
+            currWave.Enqueue(wave[i]);
+        }
+    }
+}
