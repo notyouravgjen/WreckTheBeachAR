c7c179c3d874d10ce60f2a4c2146664625ecf792
Jen <notyouravgjen@users.noreply.github.com>
2023-08-02 13:28:07 -0700

    assets

---
+++ Assets/Wreck the Beach assets/Assets/HandController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+	[SerializeField]
+	private int rotationAngle;
+
+	private bool handsDown;
+
+    void Start ()
+	{
+		handsDown = false;
+	}
+
+    public void ApplyInput()
+    {
+		if (!GameManager.instance.interactionDisabled)
+		{
+			if (!handsDown)
+			{
+				transform.Rotate(rotationAngle, 0, 0);
+				handsDown = true;
+			}
+			else
+			{
+				transform.Rotate(rotationAngle * -1, 0, 0);
+				handsDown = false;
+			}
+		}
+        else if (handsDown)
+        {
+            transform.Rotate(rotationAngle * -1, 0, 0);
+            handsDown = false;
+        }
+    }
+}
