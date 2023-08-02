d013d52f2949e18cb9224e40b27f1204cd6986a9
Jen <notyouravgjen@users.noreply.github.com>
2023-08-02 11:54:16 -0700

    more assets

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,40 +1,35 @@
-//Standard Unity/C# functionality
 using UnityEngine;
-
-//These tell our project to use pieces from the Lightship ARDK
 using Niantic.ARDK.AR;
 using Niantic.ARDK.AR.ARSessionEventArgs;
 using Niantic.ARDK.Utilities;
 using Niantic.ARDK.Utilities.Input.Legacy;
 
-//Define our main class
 public class SceneManager : MonoBehaviour
 {
-    //Variables we'll need to reference other objects in our game
-    public GameObject _ballPrefab;  //This will store the Ball Prefab we created earlier, so we can spawn a new Ball whenever we want
-    public Camera _mainCamera;  //This will reference the MainCamera in the scene, so the ARDK can leverage the device camera
+    [SerializeField]
+    private Camera _mainCamera;
+
+    [SerializeField]
+    private HandController leftHand;
+
+    [SerializeField]
+    private HandController rightHand;
+
     IARSession _ARsession;  //An ARDK ARSession is the main piece that manages the AR experience
 
-    public HandController leftHand;
-    public HandController rightHand;
-
-    // Start is called before the first frame update
     void Start()
     {
         //ARSessionFactory helps create our AR Session. Here, we're telling our 'ARSessionFactory' to listen to when a new ARSession is created, then call an 'OnSessionInitialized' function when we get notified of one being created
         ARSessionFactory.SessionInitialized += OnSessionInitialized;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //If there is no touch, we're not going to do anything
         if (PlatformAgnosticInput.touchCount <= 0)
         {
             return;
         }
 
-        //If we detect a new touch, call our 'TouchBegan' function
         var touch = PlatformAgnosticInput.GetTouch(0);
         if (touch.phase == TouchPhase.Began)
         {
@@ -52,21 +47,9 @@ public class SceneManager : MonoBehaviour
         _ARsession = args.Session;
     }
 
-    //This function will be called when the player touches the screen. For us, we'll have this trigger the shooting of our ball from where we touch.
     private void TouchBegan(Touch touch)
     {
-        //Let's spawn a new ball to bounce around our space
-        /*GameObject newBall = Instantiate(_ballPrefab);  //Spawn a new ball from our Ball Prefab
-        newBall.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));   //Set the rotation of our new Ball
-        newBall.transform.position = _mainCamera.transform.position + _mainCamera.transform.forward;    //Set the position of our new Ball to just in front of our Main Camera
-
-        //Add velocity to our Ball, here we're telling the game to put Force behind the Ball in the direction Forward from our Camera (so, straight ahead)
-        Rigidbody rigbod = newBall.GetComponent<Rigidbody>();
-        rigbod.velocity = new Vector3(0f, 0f, 0f);
-        float force = 300.0f;
-        rigbod.AddForce(_mainCamera.transform.forward * force);*/
-
-        leftHand.IsInput = true;
-        rightHand.IsInput = true;
+        leftHand.ApplyInput();
+        rightHand.ApplyInput();
     }
 }
